cb0131138d7591f24f29360d5734fecdc638a66f
David Park <sp85dav@gmail.com>
2019-09-16 12:08:46 +0700

    Car Model Updated

--- models/Car.js
+++ models/Car.js
@@ -3,8 +3,9 @@ import mongoose, { Schema } from 'mongoose';
 export const CarSchema = new Schema({
   name: String,
   price: Number,
-  categories: [String],
-  images: [String]
+  carTypes: [String],
+  images: [String],
+  condition: String
 });
 
 export const CarModel = mongoose.model('Car', CarSchema, 'cars');
